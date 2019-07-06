93f8f3eb9e2407b27577c11702aee2b8ede99d0f
hooray <hooray1998@foxmail.com>
2019-07-06 15:14:12 +0800

    使用start.sh自动运行多台设备

--- start.sh
+++ start.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-/home/itt/Coding/QtCoding/sendData/TcpClient 1 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 2 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 3 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 4 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 5 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 6 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 7 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 8 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 9 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 10 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 11 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 12 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 13 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 14 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 15 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 16 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 17 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 18 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 19 &
-/home/itt/Coding/QtCoding/sendData/TcpClient 20 &
+i=1
+while [ $i -le 20 ];do
+	/home/itt/Coding/QtCoding/sendData/TcpClient $i &
+	i=$(expr $i + 1)
+done
